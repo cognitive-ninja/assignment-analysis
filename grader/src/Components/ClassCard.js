41e271b996ad44ce08702b2007343ec7f2de5cb2
cognitive-ninja <saurabhhirugade@gmail.com>
2020-12-16 23:06:10 +0530

    ClassCard Component styling done

--- grader/src/Components/ClassCard.js
+++ grader/src/Components/ClassCard.js
@@ -1,87 +1,62 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import clsx from 'clsx';
 import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
-import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import { red } from '@material-ui/core/colors';
-// import FavoriteIcon from '@material-ui/icons/Favorite';
-import ShareIcon from '@material-ui/icons/Share';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
-import Button from '@material-ui/core/Button'
+import Button from '@material-ui/core/Button';
+import ClassCardMenu from './ClassCardMenu';
+
 const useStyles = makeStyles((theme) => ({
   root: {
-    maxWidth: 345,
+    // maxWidth: 400,
+    minWidth:300,
+    marginLeft: 40,
+    marginTop: 30,
+    backgroundColor: 'black',
+    color: 'white',
+    display: 'inline-block',
   },
-  media: {
-    height: 0,
-    paddingTop: '56.25%', // 16:9
-  },
-  expand: {
-    transform: 'rotate(0deg)',
-    marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
-      duration: theme.transitions.duration.shortest,
-    }),
-  },
-  expandOpen: {
-    transform: 'rotate(180deg)',
-  },
-  avatar: {
-    backgroundColor: red[500],
+  openButton: {
+    marginTop: 15,
+    marginBottom:15
   },
 }));
 
 export default function ClassCard() {
   const classes = useStyles();
-  const [expanded, setExpanded] = React.useState(false);
+  
 
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
-
-  return (
+return (
+  <div style={{ display: "inline-block" }}>
     <Card className={classes.root}>
-      <CardHeader
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title="FYCSE"
-        subheader="ClassTeacher Mr. Powar"
-      />
-      <CardMedia
-        className={classes.media}
-        image="/static/images/cards/paella.jpg"
-        title="Paella dish"
-      />
-      <CardActions disableSpacing>
-        <Button size="small" color="primary">
-          Open
-        </Button>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
-        <IconButton
-          className={clsx(classes.expand, {
-            [classes.expandOpen]: expanded,
-          })}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <ExpandMoreIcon />
-        </IconButton>
-      </CardActions>
-     
+      <hr></hr>
+      <ClassCardMenu className={classes.rightButton}/>      
+      <Typography>
+        Class: FYCSE<br></br>
+        Class Teacher: Mr. K. V. Madhale
+      </Typography>  
+      <Button variant="contained" size="large" color="white" className={classes.openButton}>Open</Button>
+      <hr></hr> 
     </Card>
-  );
+    <Card className={classes.root}>
+      <hr></hr>
+      <ClassCardMenu color="white"/>
+      <Typography>
+        Class: SYCSE<br></br>
+        Class Teacher: Dr. N. L. Gavankar
+      </Typography>  
+      <Button variant="contained" size="large" color="white" className={classes.openButton}>Open</Button>
+      <hr></hr> 
+    </Card>
+    <Card className={classes.root}>
+      <hr></hr>
+      <ClassCardMenu color="white"/>
+      <Typography>
+        Class: TYCSE<br></br>
+        Class Teacher: Mr. A. R. Surve
+      </Typography>  
+      <Button variant="contained" size="large" color="white" className={classes.openButton}>Open</Button>
+      <hr></hr> 
+    </Card>
+  </div>
+);
 }
