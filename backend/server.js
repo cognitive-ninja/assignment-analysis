8da2718e799968f76ad11cebf1b4384e14decab5
rushelmac <rushishelke633@gmail.com>
2020-12-18 20:14:50 +0530

    Configured MondoDB cloud Database cluster

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,18 @@
 //IMPORTS
 const express   = require('express'),
     app         = express(),
-    routes      = require('./routes/router');
+    mongoose    = require('mongoose'),
+    routes      = require('./routes/router'),
+    variables   = require('./env-variables');
 
-//Configurations
+//Configurations:
+//Rendering
 app.set("view engine","ejs");
+//Routing
 app.use(routes);
+//Database
+mongoose.connect("mongodb+srv://TeamBakchodi:" +variables.mongoDBPassword +"@assignment-analyzer.nlazz.mongodb.net/<dbname>?retryWrites=true&w=majority", {useUnifiedTopology:true, useNewUrlParser:true});
+
 
 
 const PORT = 5000 || process.env.PORT;
