4d92c5a506ca133d0be372e8abc93e1db8454d31
cognitive-ninja <saurabhhirugade@gmail.com>
2020-12-24 14:42:55 +0530

    SignUp Component added

---
+++ frontend/src/Components/SignUp.js
@@ -0,0 +1,167 @@
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+// import Link from '@material-ui/core/Link';
+import { Link } from 'react-router-dom';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import backImage from '../images/home_backg_2.jpg';
+import SignUp from './SignUp';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" to="/">
+        Assignment Analyzer
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '100vh',
+  },
+  image: {
+    backgroundColor: 'rgb(233,215,195)',
+    backgroundImage: `url(${backImage})`,
+    backgroundRepeat:'no-repeat',
+    backgroundPosition:'top',
+    backgroundSize:'cover',
+    /* color: white; */
+    /* opacity: 40%; */
+    /* margin-top: 10px; */
+    // background-position-y: 20px;
+    height: '100vh',
+
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', 
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function SignIn() {
+  const classes = useStyles();
+
+  return (
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <div className={classes.paper}>
+          <Typography component="h1" variant="h5">
+            Sign Up
+          </Typography>
+          <form className={classes.form} method="Post">
+          <div className="d-flex flex-row">
+            <div className="p-2 flex-fill">
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              autoFocus
+              name="firstname"
+              label="First Name"
+              type="text"
+              id="firstname"
+              style={{width:'260px', marginRight:'42px'}}
+            />
+            {/* </div>
+            <div className="p-2"> */}
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+            //   fullWidth
+              name="lastname"
+              label="Last Name"
+              type="text"
+              id="lastname"
+              style={{width:'260px'}}
+            />
+            </div>
+        </div>
+          
+            
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            {/* <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            /> */}
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              style={{ background: '#2E3B55' }}
+              className={classes.submit}
+            >
+              
+              <div style={{color:"white"}}>Sign Up</div>
+            </Button>
+            {/* <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item> */}
+                <Link to="/signin" variant="body2" style={{textDecoration: 'none', fontSize:'15px'}}>
+                  Already have an account? Sign In
+                </Link>
+              {/* </Grid>
+            </Grid> */}
+            <Box mt={5}>
+              <Copyright />
+            </Box>
+          </form>
+        </div>
+      </Grid>
+    </Grid>
+  );
+}
