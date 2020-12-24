246273235c5273e36ee80f5a0c763c5b45cc8461
cognitive-ninja <saurabhhirugade@gmail.com>
2020-12-24 14:44:53 +0530

    New routes added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,19 +7,27 @@ import SubjectCard from './Components/SubjectCard';
 import PostAssignment from './Components/PostAssignment';
 import HomeBackground from "./Components/Layout/HomeBackground";
 import Navbar from './Components/Layout/Navbar'
+import SignIn from "./Components/SignIn";
+import SignUp from './Components/SignUp';
+import {AuthProvider} from './Components/AuthContext';
+import createBrowserHistory from 'history/createBrowserHistory';
+
+const history = createBrowserHistory();
 function App() {
   return (
-    <>
-    <Router>
+    <AuthProvider>
+    <Router history={history}>
       <div className="App" style={{height: '1500'}}>
         <Navbar/>
         <Route exact path="/" component={HomeBackground} />
+        <Route exact path="/signin" component={SignIn} />
+        <Route exact path="/signup" component={SignUp} />
         <Route exact path="/classes" component={ClassCard} />
         <Route exact path="/classes/FYCSE" component={SubjectCard}/>
         <Route path="/postAssignment" component={PostAssignment}/>
       </div>
     </Router>
-    </>    
+    </AuthProvider>    
   );
 }
 
