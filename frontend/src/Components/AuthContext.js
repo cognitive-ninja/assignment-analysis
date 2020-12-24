8224917468014f1b86fef8ca5e713fa4a884f4c1
cognitive-ninja <saurabhhirugade@gmail.com>
2020-12-24 14:43:45 +0530

    AuthContext Component added to manage auth state

---
+++ frontend/src/Components/AuthContext.js
@@ -0,0 +1,11 @@
+import React, { useState, createContext } from 'react';
+
+export const AuthContext = createContext();
+export const AuthProvider = props => {
+    const [auth, setAuth] = useState(false);
+    return(
+        <AuthContext.Provider value={[auth, setAuth]}>
+            {props.children}
+        </AuthContext.Provider>
+    );
+}
