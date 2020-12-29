2f06d42e74a1c5a92a76b9d179f0908d0f374606
rushelmac <rushishelke633@gmail.com>
2020-12-29 23:35:04 +0530

    Created CRUD routes for classroom database

---
+++ backend/routes/classRoutes.js
@@ -0,0 +1,29 @@
+const express   = require('express'),
+    router      = express.Router(),
+    Class       = require('../models/Class');
+
+// 1. Route for creating class database.
+router.post('/class', (req, res)=> {
+    Class.create(req.class,(err, createdClass)=>{
+        if(err)
+            return res.status(500).send({
+                message: err.message || "Couldn't create database for user"
+            });
+        return res.status(200);
+    });
+});
+
+// 2. Route for fetching details of class
+
+// 3. Route for updating class details.
+
+// 4. Route for deleting class.
+router.delete('/class', (req, res)=>{
+    Class.findOneAndDelete({classname: req.classname},(err)=>{
+        if(err)
+            return res.status(500).send({
+                message: err.message || "Couldn't delete class from database"
+            });
+        return res.status(200);
+    });
+});
