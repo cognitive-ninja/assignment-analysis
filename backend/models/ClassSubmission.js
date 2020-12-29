0745257ee7197300daf7f0c9f98ea633247756ba
rushelmac <rushishelke633@gmail.com>
2020-12-29 22:37:46 +0530

    Added database model for storing answer strings.

---
+++ backend/models/ClassSubmission.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const SumbmissionSchema = mongoose.Schema({
+    class : {
+        Type    : mongoose.Schema.Types.ObjectId,
+        ref     : Class
+    },
+    assignment : [{
+        assignment_id : {
+            Type        : Number,
+            required    : true,
+            unique      : true
+        },
+        submission : [{
+            student : {
+                Type    : mongoose.Schema.Types.ObjectId,
+                ref     : User
+            },
+            answer : [{
+                q_id : Number,
+                text : String
+            }]
+        }]
+    }],
+});
+
+module.exports = mongoose.model("classSubmission", SumbmissionSchema);
