3a06ede659df2ea49337231a33ca2d364ea3b75c
rushelmac <rushishelke633@gmail.com>
2021-01-08 19:01:01 +0530

    Updated class schema for subject layer

--- backend/models/Class.js
+++ backend/models/Class.js
@@ -4,16 +4,25 @@ const User = require('./User');
 
 const ClassSchema = mongoose.Schema({
     classname   : String,
-    teacher     : {
+    owner       : {
         type : [String]
     },
     student     : {
         type : [String]
     },
-    classSubmission : [{
-        assignment_no : Number,
-        submission    : mongoose.Types.ObjectId
-    }]
+    subject : {
+        subjectname   : String,
+        teacher     : {
+            type : [String]
+        },
+        student     : {
+            type : [String]
+        },
+        classSubmission : [{
+            assignment_no : Number,
+            submission    : mongoose.Types.ObjectId
+        }]
+    }
 });
 
 module.exports = mongoose.model("Class", ClassSchema);
